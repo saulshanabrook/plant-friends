909ee9aa2b9f8a2620e133c4821208a2788d3e48
Saul Shanabrook <s.shanabrook@gmail.com>
2022-05-01 11:53:49 -0400

    Initial Commit

---
+++ plant-friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  plant-friends
+//
+//  Created by Saul Shanabrook on 5/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ plant-friends/plant_friendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  plant_friendsApp.swift
+//  plant-friends
+//
+//  Created by Saul Shanabrook on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct plant_friendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
